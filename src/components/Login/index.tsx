70eb8eb2b9d4934107ec6fcddc62b2d0285bf254
ryuhojun <rhj080471@gmail.com>
2025-10-13 23:56:07 +0900

    fix: update login button state to reflect logged out status

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from '../ui/components/Button';
 
 export function LoginButton() {
-  const isLogin = true;
+  const isLogin = false;
   return (
     <Button variant="ghost" asChild>
       <div>{isLogin ? '로그아웃' : '로그인'}</div>
