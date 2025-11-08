24ccc6c0a52712d37284d554f81a70d9dc64979a
ryuhojun <rhj080471@gmail.com>
2025-11-09 00:35:16 +0900

    feat: 프로젝트 목록 조회 api 코드 작성

--- src/api/project/apis.ts
+++ src/api/project/apis.ts
@@ -1,8 +1,13 @@
-import { post } from '@/api/client';
+import { post, get } from '@/api/client';
 import { IntakeValues } from './requests.dto';
 import { Project } from './responses.dto';
 
 export async function createProject(data: IntakeValues): Promise<Project> {
   const response = await post<{ data: Project }>('/projects', data);
+  return response.data;
+}
+
+export async function getProjects(): Promise<Project[]> {
+  const response = await get<{ data: Project[] }>('/projects');
   return response.data;
 }
